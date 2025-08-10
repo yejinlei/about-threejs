c344d42f808b23197c843beb5f23443b4fb7d52f
yeeeeeeeeee <4010896@qq.com>
2025-08-11 01:26:42 +0800

    update

---
+++ ThreeJSAssetManager/ThreeJSAssetsManager/World/sources_house.js
@@ -0,0 +1,75 @@
+export default 
+[
+    {
+            name: '4F',
+            type: 'glbModel',     
+            file:    
+            {
+                name: '4F',
+                path: 'ThreeJSAssetsManager/World/models/4F.glb',
+                position: {x: 0, y: 0, z: 0},
+                scale: 0.14,
+                rotation: {x: 0, y: 1.14, z: 0}
+            }
+    },
+    {
+        name: '3F',
+        type: 'glbModel',     
+        file:    
+        {
+            name: '3F',
+            path: 'ThreeJSAssetsManager/World/models/3F.glb',
+            position: {x: 0, y: 0, z: 0},
+            scale: 0.14,
+            rotation: {x: 0, y: 1.14, z: 0}
+        }
+    },
+    {
+        name: '2F',
+        type: 'glbModel',     
+        file:    
+        {
+            name: '2F',
+            path: 'ThreeJSAssetsManager/World/models/2F.glb',
+            position: {x: 0, y: 0, z: 0},
+            scale: 0.14,
+            rotation: {x: 0, y: 1.14, z: 0}
+        }
+    },
+    {
+        name: '1F',
+        type: 'glbModel',     
+        file:    
+        {
+            name: '1F',
+            path: 'ThreeJSAssetsManager/World/models/1F.glb',
+            position: {x: 0, y: 0, z: 0},
+            scale: 0.14,
+            rotation: {x: 0, y: 1.14, z: 0}
+        }
+    },
+    {
+        name: 'F1',
+        type: 'glbModel',     
+        file:    
+        {
+            name: 'F1',
+            path: 'ThreeJSAssetsManager/World/models/F1.glb',
+            position: {x: 0, y: 0, z: 0},
+            scale: 0.14,
+            rotation: {x: 0, y: 1.14, z: 0}
+        }
+    },
+    {
+        name: 'road',
+        type: 'glbModel',     
+        file:    
+        {
+            name: 'road',
+            path: 'ThreeJSAssetsManager/World/models/road.glb',
+            position: {x: 0, y: 0, z: 0},
+            scale: 0.14,
+            rotation: {x: 0, y: 1.14, z: 0}
+        }
+    }
+]
